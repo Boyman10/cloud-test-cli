b629a2cee8566a4cf23c5e8e97287a60da9a2adf
Your Name <you@example.com>
2021-05-27 13:45:52 +0200

    upgraded to 11

--- src/app/journal/add-entry/add-entry.component.spec.ts
+++ src/app/journal/add-entry/add-entry.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { AddEntryComponent } from './add-entry.component';
 
@@ -6,7 +6,7 @@ describe('AddEntryComponent', () => {
   let component: AddEntryComponent;
   let fixture: ComponentFixture<AddEntryComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ AddEntryComponent ]
     })
--- src/app/journal/journal.component.spec.ts
+++ src/app/journal/journal.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { JournalComponent } from './journal.component';
 
@@ -6,7 +6,7 @@ describe('JournalComponent', () => {
   let component: JournalComponent;
   let fixture: ComponentFixture<JournalComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ JournalComponent ]
     })
